55d1a61363e008e4114d35a61dd95600d683d1ed
vaishnveeyede <150323641+vaishnveeyede@users.noreply.github.com>
2025-08-06 20:25:59 +0530

    Add files via upload

---
+++ transaction.sql
@@ -0,0 +1,8 @@
+SELECT count(*) FROM sales.transactions;
+SELECT * FROM sales.transactions;
+
+
+SELECT count(*) FROM sales.transactions where market_code="Mark001";
+SELECT * FROM sales.transactions where currency="USD";
+
+
